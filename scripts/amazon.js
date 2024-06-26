aef796a75e19aed08b3f7526cb5e17fcec609e28
Ochife Ogechukwu <ochifeoge@gmail.com>
2024-06-26 20:12:47 +0100

    make the cart quantity interractive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -82,6 +82,13 @@ products.forEach((product)=>{
     
                    });
                }
-               console.log(cart);
+
+               let cartQuantity = 0
+               cart.forEach((item) =>{
+               cartQuantity += item.quantity;
+               });
+
+               document.querySelector('.js-cart-quantity')
+                .innerHTML = cartQuantity;
             });
         });
