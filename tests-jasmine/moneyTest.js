be7a1685742c54850a244552a80eb79e1c524de3
Ochife Ogechukwu <ochifeoge@gmail.com>
2024-07-10 10:25:04 +0100

    remove default tests and add money tests

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,13 @@
+import { formartCurrency } from "../scripts/utils/money.js";
+
+describe('test suit: formatCurrency', () => {
+    it('converts cents into dollars', () => {
+        expect(formartCurrency(2095)).toEqual('20.95');
+    });
+    it('works with zero', () => {
+       expect(formartCurrency(0)).toEqual('0.00') 
+    });
+    it('rounds up to the nearest cent', () => {
+        expect(formartCurrency(2000.5)).toEqual('20.01')
+    });
+});
