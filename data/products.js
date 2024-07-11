25cd1c6969d6602796316d7efe46e7aa6c1d3bd1
Ochife Ogechukwu <ochifeoge@gmail.com>
2024-07-11 12:20:26 +0100

    convert products into classes

--- data/products.js
+++ data/products.js
@@ -1,3 +1,5 @@
+import { formartCurrency } from "../scripts/utils/money.js";
+
 export function getMatchingProducts(productId) {
   let matchingItem;
 
@@ -8,6 +10,32 @@ export function getMatchingProducts(productId) {
   });
   return matchingItem;
 }
+
+  class Product {
+    id;
+    image;
+    name;
+    rating;
+    priceCents;
+
+    constructor(productDetails) {
+      this.id = productDetails.id;
+      this.image = productDetails.image;
+      this.name = productDetails.name;
+      this.rating = productDetails.rating;
+      this.priceCents = productDetails.priceCents;
+    }
+
+    getStarsUrl() {
+      return `images/ratings/rating-${this.rating.stars * 10}.png`
+    }
+
+    getPrice(){
+      return `$${formartCurrency(this.priceCents)}`
+    }
+
+  }
+
 
 export const products = [
   {
@@ -700,4 +728,7 @@ export const products = [
       "season"
     ]
   }
-];
+].map((productDetails) => {
+  return new Product(productDetails);
+});
+
