cca4612531b157314ad13e62b544468686501ff2
Ochife Ogechukwu <ochifeoge@gmail.com>
2024-07-28 22:02:52 +0100

    deleted the demo cart in cart.js

--- data/cart.js
+++ data/cart.js
@@ -8,16 +8,7 @@ loadFromStorage();
         cart = JSON.parse(localStorage.getItem('cartcopy'));
 
         if (!cart) {
-            cart = [{
-                productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-                quantity: 2,
-                deliveryOptionId: '1',
-            },
-            {
-                productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-                quantity: 1,
-                deliveryOptionId: '2'
-            }];
+            cart = [];
         }
     }
 
